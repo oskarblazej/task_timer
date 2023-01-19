f3051f7aa4c74e4647e3e7c5422169f45c30730c
oskarblazej <hampeloskarmt@gmail.com>
2023-01-19 16:59:42 +0100

    Create basic stopwatch structure

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
 ﻿using System;
-
+using System.Diagnostics; // use Stopwatch
+using System.Threading;
 
 class TimeTracker
 {
 	public static void Main()
 	{
-		Console.Write("Hello, User!");
+		SetTimer();
+		Console.WriteLine("Terminating the application");
+	}
+	static void SetTimer()
+	{
+		Stopwatch stopW = new Stopwatch();
+		stopW.Start();
+		Thread.Sleep(1000);
+		stopW.Stop();
+		TimeSpan tspan = stopW.Elapsed;
+		string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}:{3:00} ", 		tspan.Hours, tspan.Minutes, tspan.Seconds, tspan.Milliseconds / 10);
+		Console.WriteLine(" RunTime " + elapsedTime);
 	}
 }
